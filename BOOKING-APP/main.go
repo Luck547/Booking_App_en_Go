41ca9b3f2178a5fe4d48234d1ca06c4da55d5b61
Alan Lucciano Pindo <alan.lucciano@gmail.com>
2022-11-27 15:01:29 +0100

    Pasar groups of functionality to functions.

--- BOOKING-APP/main.go
+++ BOOKING-APP/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	fmt "fmt"
 	"strings"
 )
 
@@ -10,10 +10,9 @@ func main() {
 	const cantidaddeUsuariosDisponibles uint = 50
 	var usuariosqueQuedan int = 50
 	var bookings []string
-	fmt.Printf("Los usuarios son %T, los que quedan son %T, el nombre de esta app es %T\n", cantidaddeUsuariosDisponibles, cantidaddeUsuariosDisponibles, appName)
-	fmt.Printf("Bienvenido a la app de reserva de %v\n", appName)
-	fmt.Printf("Tenemos un total de %v usuarios disponibles, y van quedando solo %v libres.\n", cantidaddeUsuariosDisponibles, usuariosqueQuedan)
-	fmt.Println("Reserva tu nombre de usuario aqui.")
+
+	greetUsers(appName, cantidaddeUsuariosDisponibles, usuariosqueQuedan)
+
 	for {
 		var userName1 string
 		var userName2 string
@@ -21,6 +20,7 @@ func main() {
 		var usuarios int
 		var nombresdeUsuarios string
 		// Solicita nombre de usuario al usuario
+		printUserName1(bookings)
 		fmt.Println("tipea tu primer nombre")
 		fmt.Scan(&userName1)
 		fmt.Println("tipea tu apellido")
@@ -32,31 +32,65 @@ func main() {
 		fmt.Println("Ingresa tu nombre de usuario a reservar")
 		fmt.Scan(&nombresdeUsuarios)
 
-		isValidName := len(userName1) >= 3 && len(userName2) >= 3
-		isValidEmail := strings.Contains(email, "@")
-		isValidUsuarios := usuarios > 0 && usuarios <= usuariosqueQuedan
-		isValidNombresdeUsuarios := len(nombresdeUsuarios) >= 5
-		//tipo := "Usuario publico" OR "Usuario privado"
-		//isValidType := tipo == "Usuario publico" || tipo == "Usuario privado"
+		isValidName, isValidEmail, isValidUsuarios, isValidNombresdeUsuarios := validateUserInput(userName1, userName2, email, usuarios, nombresdeUsuarios, usuariosqueQuedan)
+
 		if isValidName && isValidEmail && isValidUsuarios && isValidNombresdeUsuarios {
 			usuariosqueQuedan = usuariosqueQuedan - usuarios
 			bookings = append(bookings, userName1+" "+userName2)
 			fmt.Printf("Gracias %v %v por reservar %v Nombres de usuario.\nEl primero es %v. Te llagara un correo de confirmacion a %v\n", userName1, userName2, usuarios, nombresdeUsuarios, email)
 			fmt.Printf("Solo quedan %v nombres de usuario en %v,. Gracias!\n", usuariosqueQuedan, appName)
-			var userName []string
-			for _, booking := range bookings {
-				var names = strings.Fields(booking)
-				userName = append(userName, names[0])
-			}
-			fmt.Printf("These are all the bookings: %v\n", userName)
+			// Call a function print userName1
+			printUserName1(bookings)
+
+			fmt.Printf("These are all the bookings: %v\n", userName1)
 			if usuariosqueQuedan == 0 {
 				// Finalizar programa
 				fmt.Println("Se acabaron los nombres de usuario, reinicia el programa")
 				break
 			}
 		} else {
-			//fmt.Printf("Solo tenemos %v nombres de usuario, por eso no puedes reservar %v nombres de usuario\n", usuariosqueQuedan, usuarios)
-			fmt.Println("Has ingresados datos invalidos, prueba de nuevo")
+			if !isValidName {
+				fmt.Println("El nombre es invalido")
+			}
+			if !isValidEmail {
+				fmt.Println("El EMAIL es invalido")
+			}
+			if !isValidUsuarios {
+				fmt.Println("El numero de usuarios es invalido")
+			}
+			if !isValidNombresdeUsuarios {
+				fmt.Println("El nombre de usuario es invalido")
+			} else {
+				fmt.Println("Reservado")
+			}
+
 		}
 	}
+}
+
+func greetUsers(appName string, cantidaddeUsuariosDisponibles uint, usuariosqueQuedan int) {
+	fmt.Printf("Los usuarios son %T, los que quedan son %T, el nombre de esta app es %T\n", cantidaddeUsuariosDisponibles, cantidaddeUsuariosDisponibles, appName)
+	fmt.Printf("Bienvenido a la app de reserva de %v\n", appName)
+	fmt.Printf("Tenemos un total de %v usuarios disponibles, y van quedando solo %v libres.\n", cantidaddeUsuariosDisponibles, usuariosqueQuedan)
+	fmt.Printf("Bienvenido a la reserva de %v", appName)
+	fmt.Println("Reserva tu nombre de usuario aqui.")
+}
+
+func printUserName1(bookings []string) []string {
+	var userName1 []string
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		userName1 = append(userName1, names[0])
+	}
+	return userName1
+}
+
+func validateUserInput(userName1 string, userName2 string, email string, usuarios int, nombresdeUsuarios string, usuariosqueQuedan int) (bool, bool, bool, bool) {
+	isValidName := len(userName1) >= 3 && len(userName2) >= 3
+	isValidEmail := strings.Contains(email, "@")
+	isValidUsuarios := usuarios > 0 && usuarios <= usuariosqueQuedan
+	isValidNombresdeUsuarios := len(nombresdeUsuarios) >= 5
+	return isValidName, isValidEmail, isValidUsuarios, isValidNombresdeUsuarios
+	//tipo := "Usuario publico" OR "Usuario privado"
+	//isValidType := tipo == "Usuario publico" || tipo == "Usuario privado"
 }
